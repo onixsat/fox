be0f6d1ceb5d75062f56ff83072609c1482ddd9b
onixsat <163943278+onixsat@users.noreply.github.com>
2026-03-12 21:14:47 +0000

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@
 set -euo pipefail
 
 sudo apt update -y
-sudo apt install -y git nano wget dos2unix
+sudo apt install -y git nano wget dos2unix php8.3-cli
 
 git clone https://github.com/onixsat/fox.git
 dos2unix fox/*
