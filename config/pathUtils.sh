09c2f45220ef09ad9b1c128e574fc2a7725afc19
onixsat <163943278+onixsat@users.noreply.github.com>
2026-03-12 22:50:11 +0000

    Update pathUtils.sh

--- config/pathUtils.sh
+++ config/pathUtils.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
-# http://www.ostricher.com/2014/10/the-right-way-to-get-the-directory-of-a-bash-script/
-
-# Gets absolute path to the script that was directly run
 function getActiveScriptDir() {
     src="$0"
     while [ -h "$src" ]; do
@@ -12,8 +9,6 @@ function getActiveScriptDir() {
     cd -P "$( dirname "$src" )"
     pwd
 }
-
-# Gets absolute path to the parent dir of this file
 function getProjectRoot() {
     src="${BASH_SOURCE[0]}"
     while [ -h "$src" ]; do
@@ -24,24 +19,14 @@ function getProjectRoot() {
     cd -P "$( dirname $( dirname "$src" ) )"
     pwd
 }
-
-# Gets absolute path to config dir
 function getConfigDir() {
     local rootDir="$( getProjectRoot )"
-    # local configDir="$rootDir/templates/config"
-    # local localConfigDir="$rootDir/local/config"
-    # [[ -d "$localConfigDir" ]] && configDir="$localConfigDir"
-    # echo "$configDir"
     echo "$rootDir/config"
 }
-
-# Load config file from proper config dir
 function srcConfigFile() {
     local file="$1"
     source "$( getConfigDir )/$file"
 }
-
-# Load lib file from proper libs dir
 function srcLibFile() {
     local file="$1"
     source "$( getProjectRoot )/config/$file"
