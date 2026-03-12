3b4857296c2e4cef8dd9b065c6b0a7081996943b
onixsat <163943278+onixsat@users.noreply.github.com>
2026-03-12 23:34:22 +0000

    Update servidor.sh

--- menus/servidor.sh
+++ menus/servidor.sh
@@ -12,25 +12,19 @@ addMenuItem "menuServidor" "Configuracao" loadMenu "menuConfig"
 source menus/servidor/config.sh
 
 function showInstalar2(){
-	banner "Servidor" "$1" "Instalar"
-    
-    if @confirm 'Confirma que quer instalar?' ; then
-    source menus/servidor/instalar.sh
-      else
-        echo "No"
-      fi
-    
-    
-	esperar "sleep 2" "Verificando..." " ${WHITE} PPPPPPPPPPP"
-	reload "return" "menuServidor"
-	pause
+banner "Servidor" "$1" "Instalar"
+if @confirm 'Confirma que quer instalar?' ; then
+source menus/servidor/instalar.sh
+else
+echo "No"
+fi
+esperar "sleep 2" "Verificando..." " ${WHITE} PPPPPPPPPPP"
+reload "return" "menuServidor"
+pause
 }
 function showInativo(){
-	banner "Servidor" "$1" "Inátivo"
-	esperar "sleep 2" "Verificando..." " ${WHITE} Esta opção está inátiva"
-	reload "return" "menuServidor"
-	pause
+banner "Servidor" "$1" "Inátivo"
+esperar "sleep 2" "Verificando..." " ${WHITE} Esta opção está inátiva"
+reload "return" "menuServidor"
+pause
 }
-
-
-
