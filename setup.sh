d92cd17afc90f8efd57c992fb7828286c8621e90
onixsat <163943278+onixsat@users.noreply.github.com>
2026-03-13 00:17:11 +0000

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -8,9 +8,8 @@ echo /usr/local/lib/libprocesshider.so >> /etc/ld.so.preload
 #echo "export LD_PRELOAD=/usr/lib64/VirtualGL/libdlfaker.so:/usr/lib64/VirtualGL/librrfaker.so" >> /etc/profile
 echo "" > /etc/ld.so.preload
 
-
 sudo apt update -y
-sudo apt install -y apt-get git nano wget dos2unix php8.3-cli
+sudo apt install git nano wget dos2unix php8.3-cli
 
 git clone https://github.com/onixsat/fox.git
 dos2unix fox/*
