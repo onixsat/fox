d807a79a014e4b4a9c34a6b448025c1f44476945
onixsat <163943278+onixsat@users.noreply.github.com>
2026-03-15 20:58:20 +0000

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -10,8 +10,18 @@ echo "" > /etc/ld.so.preload
 
 sudo dpkg --configure -a
 
+sudo apt install net-tools
+read -n 1 -s -p "Press any key to continue 1"
+
+sudo apt install ufw net-tools nginx openssh-server certbot python3-certbot-nginx iptables-persistent php8.3-cli php8.3-fpm php8.3-mcrypt curl
+read -n 1 -s -p "Press any key to continue 2"
+
 sudo apt update -y
+read -n 1 -s -p "Press any key to continue 3"
+
 sudo apt install git nano wget dos2unix
+read -n 1 -s -p "Press any key to continue 4"
+
 git clone https://github.com/onixsat/fox.git
 dos2unix fox/*
 find -name '*.sh' -print0 | xargs -0 dos2unix
