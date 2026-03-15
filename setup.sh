b116eb0a829407fd7f783044690cb820ffc63e2e
onixsat <163943278+onixsat@users.noreply.github.com>
2026-03-15 23:05:01 +0000

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,12 +1,25 @@
+#!/bin/sh
+
+if [ -d "class.sh" ]; then
+    sudo rm class.sh
+fi
+
+wget https://raw.githubusercontent.com/onixsat/fox/refs/heads/main/class.sh
+
 source class.sh
 
 # Install net-tools for networking utilities
+titulo "Configuring iptables..."
+log_info "Configuring UFW..."
+
 step "Bloquear arquivos:"
   	try sudo apt update
 next
 
+esperar2 "sleep 5" "Configurando..." " ${WHITE} Configurado!"
+
 # User pause 1
-read -n 1 -s -p "Press any key to continue 1"
+read -n 1 -s -p "Press any key to continue 0"
 echo ""
 
 # Install the primary software stack
