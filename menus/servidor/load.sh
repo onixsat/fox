93b874ccc1006600e2deabbc0b1923b8f88ee4e5
onixsat <163943278+onixsat@users.noreply.github.com>
2026-03-29 08:47:58 +0100

    Implement preloading function in load.sh
    Added preloading function for better user feedback during installation.

--- menus/servidor/load.sh
+++ menus/servidor/load.sh
@@ -1,4 +1,37 @@
 #!/bin/bash
+function preloading(){
+  CINZA="$(tput setaf 8)"
+  CHECK_MARK="\033[0;32m\xE2\x9C\x94\033[0m"
+  CHECK_SYMBOL='\u2713'
+  X_SYMBOL='\u2A2F'
+  local done=${3:-'Atualizado'}
+  local msg=$2
+  eval $1 >/tmp/execute-and-wait.log 2>&1 &
+  pid=$!
+  delay=0.05
+  frames=('\u280B' '\u2819' '\u2839' '\u2838' '\u283C' '\u2834' '\u2826' '\u2827' '\u2807' '\u280F')
+  echo "$pid" >"/tmp/.spinner.pid"
+  tput civis
+  index=0
+  framesCount=${#frames[@]}
+  while [ "$(ps a | awk '{print $1}' | grep $pid)" ]; do
+    printf "${YELLOW}${frames[$index]}${NC} ${GREEN}${msg}${NC}"
+
+    let index=index+1
+    if [ "$index" -ge "$framesCount" ]; then
+      index=0
+    fi
+
+    printf "\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b"
+    sleep $delay
+  done
+
+  echo -e "\b\\r${CHECK_MARK}${CINZA} ${done}!   "
+  echo -e ""
+  unset GLOBAL_TIME
+  unset start_time2
+}
+
 function cmd1() {
 #    echo -n "Updating1..."
     sudo apt update -y >/dev/null 2>&1 &
@@ -25,7 +58,7 @@ step "Step1: "
 try cmd1
 next
 
-esperar "sleep 5" "Instalando..." " ${WHITE} Instalado em $GLOBAL_TIME"
+preloading "sleep 5" "Instalando..." " ${WHITE} Instalado em $GLOBAL_TIME"
 
 read -n 1 -s -p "Press final"
 echo ""
