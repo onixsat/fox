e93330169106221930916b51a41c3f0609125312
onixsat <163943278+onixsat@users.noreply.github.com>
2026-03-29 07:32:32 +0100

    Refactor load.sh to improve logging and structure

--- menus/servidor/load.sh
+++ menus/servidor/load.sh
@@ -1,52 +1,21 @@
 #!/bin/bash
-
-function esperar2(){
-  CINZA="$(tput setaf 8)"
-  CHECK_MARK="\033[0;32m\xE2\x9C\x94\033[0m"
-  CHECK_SYMBOL='\u2713'
-  X_SYMBOL='\u2A2F'
-  local done=${3:-'Atualizado'}
-  local msg=$2
-  eval $1 >/tmp/execute-and-wait.log 2>&1 &
-  pid=$!
-  delay=0.05
-  frames=('\u280B' '\u2819' '\u2839' '\u2838' '\u283C' '\u2834' '\u2826' '\u2827' '\u2807' '\u280F')
-  echo "$pid" >"/tmp/.spinner.pid"
-  tput civis
-  index=0
-  framesCount=${#frames[@]}
-  while [ "$(ps a | awk '{print $1}' | grep $pid)" ]; do
-    printf "${YELLOW}${frames[$index]}${NC} ${GREEN}${msg}${NC}"
-    let index=index+1
-    if [ "$index" -ge "$framesCount" ]; then
-      index=0
-    fi
-    printf "\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b"
-    sleep $delay
-  done
-  echo -e "\b\\r${CHECK_MARK}${CINZA} ${done}!   "
-  echo -e ""
-  read -n 1 -s -p "Press any key to continue."
-  echo ""
-  clear
-}
-
 function cmd1() {
-    log_info "Updating and upgrading system packages..."
-    echo "666"
+    log_info "Updating1..."
     sudo apt update -y
     sudo apt upgrade -y
 }
 
 function cmd0(){ 
-    echo -n "texto"
+    log_info "Updating2..."
 	sleep 3
 }
 clear
 
 log_info "Instalar..."
-step "Step1:"
+step "Step1: "
 	try cmd0
+	echo ""
+	sleep 5
 	try cmd1
 next	
 	
