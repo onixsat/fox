3a0506476f93206fa89e2aa2b429ce491539bfb5
onixsat <163943278+onixsat@users.noreply.github.com>
2026-03-29 11:40:16 +0100

    Update load.sh

--- menus/servidor/load.sh
+++ menus/servidor/load.sh
@@ -1,37 +1,5 @@
 #!/bin/bash
 clear
-function preloading(){
-  CINZA="$(tput setaf 8)"
-  CHECK_MARK="\033[0;32m\xE2\x9C\x94\033[0m"
-  CHECK_SYMBOL='\u2713'
-  X_SYMBOL='\u2A2F'
-  local done=${3:-'Atualizado'}
-  local msg=$2
-  eval $1 >/tmp/execute-and-wait.log 2>&1 &
-  pid=$!
-  delay=0.05
-  frames=('\u280B' '\u2819' '\u2839' '\u2838' '\u283C' '\u2834' '\u2826' '\u2827' '\u2807' '\u280F')
-  echo "$pid" >"/tmp/.spinner.pid"
-  tput civis
-  index=0
-  framesCount=${#frames[@]}
-  while [ "$(ps a | awk '{print $1}' | grep $pid)" ]; do
-    printf "${YELLOW}${frames[$index]}${NC} ${GREEN}${msg}${NC}"
-
-    let index=index+1
-    if [ "$index" -ge "$framesCount" ]; then
-      index=0
-    fi
-
-    printf "\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b"
-    sleep $delay
-  done
-
-  echo -e "\b\\r${CHECK_MARK}${CINZA} ${done}!   "
-  echo -e ""
-  unset GLOBAL_TIME
-  unset start_time2
-}
 function executar() {
 	arg1=$1
 	arg2=$2
@@ -40,9 +8,10 @@ function executar() {
 	next
 }
 function app_0() {
-    sudo apt update -y
+    sudo apt update -mm
     sudo apt upgrade -y
 }
+
 function app_1(){
   ufw allow 80/tcp 
 }
@@ -55,4 +24,4 @@ executar "Step0: " "app_0"
 executar "Step1: " "app_1"
 executar "Step2: " "app_2"
 
-preloading "sleep 3" "Instalando..." " ${WHITE} Instalado em $GLOBAL_TIME"
+echo " ${WHITE} Instalado em $GLOBAL_TIME"
