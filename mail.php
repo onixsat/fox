5b546a865f06c8899d5d17072387c674d7753420
onixsat <163943278+onixsat@users.noreply.github.com>
2026-07-04 02:11:13 +0100

    Atualizar o mail.php

--- mail.php
+++ mail.php
@@ -16,8 +16,16 @@ $mail->isSMTP();
 $mail->IsHTML(true);
 $mail->CharSet = "UTF-8";
 $mail->From = "noreply@ospro.pt";
-$mail->addAddress('ospro1988@gmail.com');
-$mail->Body="Test email sent successfully.";
+$mail->addAddress('test-g8fhy0vl8@srv1.mail-tester.com');
+$mail->Body="Test email sent successfully.Questions? Contact our support team: unsub@example.com?subject=unsubscribe
+Acme, Inc. 123 Market Street, San Francisco, CA 94103
+Privacy: privacyUrl
+Terms: termsUrl
+
+List-Unsubscribe: https://form.flodesk.com/preferences/V2RYYlBGMmJYRmNTRzYtOEomTmtNUy1lS0hhVnRBbmt0Ji1qU09XaVhzZ1l6Z1ctdk5ROVRUMGdIS2xYd2paLklSU2NUQzR0SkI5UXpqMThGU05tTk8zalVBa09Iay1aSg/unsubscribe?id=xyz
+Unsubscribe: https://form.flodesk.com/preferences/V2RYYlBGMmJYRmNTRzYtOEomTmtNUy1lS0hhVnRBbmt0Ji1qU09XaVhzZ1l6Z1ctdk5ROVRUMGdIS2xYd2paLklSU2NUQzR0SkI5UXpqMThGU05tTk8zalVBa09Iay1aSg/unsubscribe?id=xyz
+
+List-Unsubscribe-Post: List-Unsubscribe=One-Click.";
 $mail->Subject="Test Email";
 $mail->SMTPDebug = 1;
 $mail->Debugoutput = function($str, $level) {echo "debug level $level; message: $str"; echo "<br>";};
